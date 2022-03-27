8efe30a1c4339b633ec14e393f1c50576c103a61
AnatoliiWorkmail <jp.komod.su@gmail.com>
2022-03-27 16:46:28 +0300

    task-1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,27 @@
+const buttonStart = document.querySelector('[data-start]');
+const buttonStop = document.querySelector('[data-stop]');
 
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+buttonStop.disabled = true;
+console.log(buttonStart, buttonStop);
+
+let timerId = 0;
+function onStartClick() {
+  buttonStart.disabled = true;
+  buttonStop.disabled = false;
+  timerId = setInterval(changeBackground, 1000);
+}
+function onStopClick() {
+  buttonStart.disabled = !true;
+  buttonStop.disabled = !false;
+  clearInterval(timerId);
+}
+buttonStart.addEventListener('click', onStartClick);
+buttonStop.addEventListener('click', onStopClick);
+
+function changeBackground() {
+  document.body.style.backgroundColor = getRandomHexColor();
+}
+console.log(buttonStart.disabled);
